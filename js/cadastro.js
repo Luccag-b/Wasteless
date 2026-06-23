b5ddd200cc4dcf41438d82af6e655dec14adc4eb
Gustavo B Alves <157503575+Gustavobalves@users.noreply.github.com>
2026-06-22 21:24:00 -0300

    Update cadastro.js
    Signed-off-by: Gustavo B Alves <157503575+Gustavobalves@users.noreply.github.com>

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,22 @@
-function cadastrarUsuario(){
-    alert("Usuário cadastrado com sucesso!");
+function cadastrarUsuario() {
+    var nome = document.getElementById("nome").value;
 
+    if (nome == "") {
+        alert("Digite nome e sobrenome");
+        return;
+    }
+
+    var partes = nome.split(" ");
+
+    if (partes.length < 2) {
+        alert("Digite nome e sobrenome");
+        return;
+    }
+
+    if (partes[0].length < 2 || partes[1].length < 2) {
+        alert("Nome e sobrenome devem ter pelo menos 2 letras");
+        return;
+    }
+
+    alert("Usuário cadastrado com sucesso!");
 }
